61ab3240e392c2d8c5deb92b76f192bc94673857
Jobin Sabu <jsabu300@gmail.com>
2018-04-09 18:12:38 +0530

    First Commit
    All files included.

---
+++ java_s.js
@@ -0,0 +1,443 @@
+
+document.getElementById("1C").addEventListener("click", function(){
+    var audio = new Audio('media/261-C.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1C";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+
+
+});
+
+
+document.getElementById("1Cs").addEventListener("click", function(){
+	var audio = new Audio('media/277-C-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1Cs";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("1D").addEventListener("click", function(){
+    var audio = new Audio('media/293-D.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1D";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("1Ds").addEventListener("click", function(){
+	var audio = new Audio('media/311-D-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1Ds";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+
+document.getElementById("1E").addEventListener("click", function(){
+    var audio = new Audio('media/329-E.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1E"
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("1F").addEventListener("click", function(){
+	var audio = new Audio('media/349-F.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1F";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+
+document.getElementById("1Fs").addEventListener("click", function(){
+    var audio = new Audio('media/369F-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1Fs";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("1G").addEventListener("click", function(){
+	var audio = new Audio('media/391-G.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1G";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+
+document.getElementById("1Gs").addEventListener("click", function(){
+    var audio = new Audio('media/415-G-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "1Gs";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2A").addEventListener("click", function(){
+	var audio = new Audio('media/440-A.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2A";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2As").addEventListener("click", function(){
+    var audio = new Audio('media/466-A-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2As";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2B").addEventListener("click", function(){
+	var audio = new Audio('media/495-B.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2B";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2C").addEventListener("click", function(){
+    var audio = new Audio('media/523-C.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2C";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2Cs").addEventListener("click", function(){
+	var audio = new Audio('media/545-C-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2Cs";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+
+document.getElementById("2D").addEventListener("click", function(){
+    var audio = new Audio('media/587-D.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2D";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2Ds").addEventListener("click", function(){
+	var audio = new Audio('media/622-D-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2Ds";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+
+document.getElementById("2E").addEventListener("click", function(){
+    var audio = new Audio('media/659-E.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2E";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2F").addEventListener("click", function(){
+	var audio = new Audio('media/698-F.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2F";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2Fs").addEventListener("click", function(){
+	var audio = new Audio('media/698-F-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2Fs";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2G").addEventListener("click", function(){
+    var audio = new Audio('media/783-G.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2G";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("2Gs").addEventListener("click", function(){
+	var audio = new Audio('media/830-G-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "2Gs";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+document.getElementById("3A").addEventListener("click", function(){
+    var audio = new Audio('media/880-A.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "3A";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("3As").addEventListener("click", function(){
+	var audio = new Audio('media/932-A-sharp.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "3As";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+document.getElementById("3B").addEventListener("click", function(){
+	var audio = new Audio('media/987-B.mp3');
+	audio.play();
+
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	audio.volume=vol/10;
+
+	setTimeout(function(){audio.pause(); }, 1500);
+	audio.currentTime = 0;
+	document.getElementById("mn_dis").innerHTML = "3B";
+	setTimeout(function(){document.getElementById("mn_dis").innerHTML = " "; }, 500);
+});
+
+
+document.getElementById("vup").addEventListener("click", function(){
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	if(vol<5){
+	vol+=1;
+	document.getElementById("vd").innerHTML=vol;
+	}
+});
+
+document.getElementById("vdown").addEventListener("click", function(){
+	var vol = parseInt(document.getElementById("vd").innerHTML);
+	if(vol>0){
+	vol-=1;
+	document.getElementById("vd").innerHTML=vol;
+	}
+});
+
+document.getElementById("tup").addEventListener("click", function(){
+	var temp = parseInt(document.getElementById("td").innerHTML);
+	if(temp<5){
+	temp+=1;
+	document.getElementById("td").innerHTML=temp;
+	}
+});
+
+document.getElementById("tdown").addEventListener("click", function(){
+	var temp = parseInt(document.getElementById("td").innerHTML);
+	if(temp>1){
+	temp-=1;
+	document.getElementById("td").innerHTML=temp;
+	}
+});
+
+function play(){
+
+
+var audio = new Audio('media/tempo.mp3');
+var vol = audio.volume;
+var temp = audio.playbackRate;
+
+
+document.getElementById("start").addEventListener("click", function(){
+	if(audio.play()){
+		audio.currentTime = 0;
+		var v = parseInt(document.getElementById("vd").innerHTML);
+		audio.volume = (v/10)*2;
+
+		var t = parseInt(document.getElementById("td").innerHTML);
+		audio.volume = t;
+		audio.playbackRate = t/10;
+	}
+	else{
+		var v = parseInt(document.getElementById("vd").innerHTML);
+		audio.volume = (v/10)*2;
+
+		var t = parseInt(document.getElementById("td").innerHTML);
+		audio.playbackRate = t/10;
+
+		audio.play();
+	}
+
+});
+
+document.getElementById("stop").addEventListener("click", function(){
+	audio.pause();
+	audio.currentTime = 0;
+	audio.playbackRate = 1;
+	audio.volume = 1;
+});
+
+
+document.getElementById("vup").addEventListener("click", function(){
+	if(vol<1){
+	vol +=0.2;
+	audio.volume = vol;
+	}
+});
+
+document.getElementById("vdown").addEventListener("click", function(){
+	if(vol>0.1){
+	vol -=0.2;
+	audio.volume = vol;
+	}
+});
+
+document.getElementById("tup").addEventListener("click", function(){
+	if(temp<1.2){
+	temp +=0.1;
+	audio.playbackRate = temp;
+	}
+});
+
+document.getElementById("tdown").addEventListener("click", function(){
+	if(temp>0.8){
+	temp -=0.1;
+	audio.playbackRate = temp;
+	}
+});
+
+}
+
+play();
